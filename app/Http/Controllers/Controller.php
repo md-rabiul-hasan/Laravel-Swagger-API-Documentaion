a614394560b10fd54d9f45bd718e6ef1f6690d11
mdrabiulhasan <mdrabiulhasan.me@gmail.com>
2022-05-26 14:39:08 +0600

    @OA\Info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,30 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Laravel OpenApi Demo Documentation",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *         name="API Support",
+ *         url="http://www.example.com/support",
+ *         email="support@example.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ * @OA\Server(
+ *      url="http://127.0.0.1:8000/api",
+ *      description="Local Server"
+ * )
+ * @OA\Tag(
+ *     name="Projects",
+ *     description="API Endpoints of Projects"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
